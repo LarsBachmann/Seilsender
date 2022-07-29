291b67e57df51855fa3e3919fded0136ac6dbf7d
LarsBachmann <NN5xy3qZ7bQ7MpmOsL22>
2022-07-29 16:03:15 +0200

    Update launch_darkice.sh
    Fixed the relative paths and commented out the logging parts.

--- darkice/launch_darkice.sh
+++ darkice/launch_darkice.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-date 2>&1 >> ./darkice.log &
-arecord -l 2>&1 >> ./darkice.log &
-sleep 30
-XDG_RUNTIME_DIR=/run/user/1000 /usr/bin/darkice -c ./darkice.cfg
+dir_path=$(dirname $(realpath $0)) #get the current directory for relative paths
+# date 2>&1 >> $dir_path/darkice.log &
+sleep 30 #wait for 30 seconds to make sure everything is booted up
+# arecord -l 2>&1 >> $dir_path/darkice.log &
+XDG_RUNTIME_DIR=/run/user/1000 /usr/bin/darkice -c $dir_path/darkice.cfg # 2>&1 >> $dir_path/darkice.log
